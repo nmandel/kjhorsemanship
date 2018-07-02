9174c550d7a8102148e76c1ddc6eeb1851e72eb1
Nick Mandel <nrmandel@ucdavis.edu>
2018-07-01 21:55:41 -0700

    Comment out app.get

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var path = require('path');
 
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
