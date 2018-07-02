17d6d29f0ed107857523d075c92807fcc697afa9
Nick Mandel <nrmandel@ucdavis.edu>
2018-07-01 21:59:54 -0700

    Add process.env.PORT to app.listen

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.get('/', function (req, res) {
 //   res.sendFile(path.join(__dirname + '/index.html'));
 // });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
