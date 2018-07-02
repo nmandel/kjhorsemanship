c4ccb0925415f2c2ee86c29eb63694cf0845b7da
Nick Mandel <nrmandel@ucdavis.edu>
2018-07-01 21:40:39 -0700

    Get images and css working

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
